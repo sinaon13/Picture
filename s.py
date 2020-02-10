1f0246ab2a77819ffb1d01bec7d014c6cbe5013b
3060756538 <3060756538@heli3.local>
2020-02-10 11:45:31 +0330

    sinaon

--- s.py
+++ s.py
@@ -1,37 +1,31 @@
 from PIL import Image
 img = Image.open('1.jpg')
 n=int(input("percent of opacue (multiples of 10) : "))
-def sm(img,ij,fl,x,y):
+def sm(img,j,x,y):
     t=(0,0,0)
     p=-1
+    l=-1
     for i in range(j):
+        l+=1
         for k in range(j):
             p+=1
-            t+=img.getpixel(x,y+i-p)
-            t+=img.getpixel(x+i-p,y)
-    
-def op(img):
+            g1=t[0]            
+            g2=t[1]
+            g3=t[2]
+            t=(g1+img.getpixel((x+p-j,y+l-j))[0],g2+img.getpixel((x+p-j,y+l-j))[1],g3+img.getpixel((x+p-j,y+l-j))[2])
+    g1=t[0]/(j**2)          
+    g2=t[1]/(j**2) 
+    g3=t[2]/(j**2) 
+    t=(g1,g2,g3)
+    return t
+def op(img,n):
     weight = img.size[0]
     height = img.size[1]
-    j=0
-    fl=0
     for x in range(weight-1):
-        l = []
         for y in range(height-1):
-            if (x>1 and y>1) or (x<weight-1 and y>1) or (x>1 and y<height-1) or (x<weight-1 and y<height-1):
-                t = img.getpixel((x+1, y-1))+img.getpixel((x-1, y+1))+img.getpixel((x-1, y-1))+img.getpixel((x+1, y))+img.getpixel((x, y+1))+img.getpixel((x, y-1))+img.getpixel((x-1, y))+img.getpixel((x+1, y+1))
-            r=0
-            b=0
-            g=0
-            for i in range(len(t)):
-                if i%3==1:
-                    r+=t[i]
-                if i%3==2:
-                    g+=t[i]
-                if i%3==0:
-                    b+=t[i]
-            r=int(r/8)
-            g=int(g/8)
-            b=int(b/8)
+            if (x>n and y>n) or (x<weight-n and y>n) or (x>n and y<height-n) or (x<weight-n and y<height-n):
+                (r,g,b)=sm(img,n,x,y)
             img.putpixel((x, y), (r, g, b))
+    return(img)
+img=op(img,n)
 img.save('2.jpg')
